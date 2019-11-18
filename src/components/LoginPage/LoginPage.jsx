a99d04e16c74966440a6649b03779ff167a0202d
Lucas Natoli <lucas.natoli@gmail.com>
2019-11-17 23:44:36 -0300

    LoginPage revamp

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,89 +1,75 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Input, Icon, Card } from 'antd';
-import { connect } from 'react-redux';
+import { Button, Checkbox, Form, Icon, Input } from 'antd';
+import './LoginPage.css'
+import { accountActions } from '../../actions'
+import { connect } from 'react-redux'
 
-import { accountActions } from '../../actions';
-
-class LoginPage extends React.Component {
-    constructor(props) {
-        super(props);
-
-        // reset login status
-        this.props.dispatch(accountActions.logout());
-
-        this.state = {
-            username: '',
-            password: '',
-            submitted: false
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+class LoginForm extends React.Component {
+    state = {
+        "email": "",
+        "password": ""
     }
 
-    handleChange(e) {
-        const { name, value } = e.target;
-        this.setState({ [name]: value });
-    }
-
-    handleSubmit(e) {
+    handleSubmit = e => {
         e.preventDefault();
-
-        this.setState({ submitted: true });
-        const { username, password } = this.state;
-        const { dispatch } = this.props;
-        if (username && password) {
-            dispatch(accountActions.login(username, password));
-        }
-    }
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                const { dispatch } = this.props;
+                dispatch(accountActions.login(values.email, values.password))
+            }
+        });
+    };
 
     render() {
-        const { loggingIn } = this.props;
-        const { username, password, submitted } = this.state;
+        const { getFieldDecorator } = this.props.form;
         return (
-            <Card className="login-card">
-                <h2>Iniciar Sesion</h2>
-                <form name="form" onSubmit={this.handleSubmit}>
-                    <div className={'form-group mb-1' + (submitted && !username ? ' has-error' : '')}>
-                        <label htmlFor="username">Email</label>
+            <Form onSubmit={this.handleSubmit} className="login-form">
+                <Form.Item>
+                    {getFieldDecorator('email', {
+                        rules: [{ required: true, message: 'Please input your email!' }],
+                    })(
                         <Input
-                            placeholder="Ingrese su Email"
                             prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                            onChange={this.handleChange}
-                            name="username"
-                            value={username}
-                        />
-                        {submitted && !username &&
-                            <div className="help-block">Ingrese un Email valido</div>
-                        }
-                    </div>
-                    <div className={'form-group mb-1' + (submitted && !password ? ' has-error' : '')}>
-                        <label htmlFor="password">Contraseña</label>
-                        <Input.Password placeholder="input password" name="password" value={password} onChange={this.handleChange}/>
-                        {submitted && !password &&
-                            <div className="help-block">Ingrese su contraseña</div>
-                        }
-                    </div>
-                    <div className="form-group mt-2 ta-r">
-                        {loggingIn &&
-                            <img alt="" src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-                        }
-                        <Link to="/register" className="ant-btn ant-btn-default mr-1 ripple">Crear Cuenta</Link>
-                        <button type="submit" className="ant-btn ant-btn-primary">Iniciar Sesion</button>
-                    </div>
-                </form>
-            </Card>
+                            placeholder="Email"
+                        />,
+                    )}
+                </Form.Item>
+                <Form.Item>
+                    {getFieldDecorator('password', {
+                        rules: [{ required: true, message: 'Please input your Password!' }],
+                    })(
+                        <Input
+                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            type="password"
+                            placeholder="Password"
+                        />,
+                    )}
+                </Form.Item>
+                <Form.Item>
+                    {getFieldDecorator('remember', {
+                        valuePropName: 'checked',
+                        initialValue: false,
+                    })(<Checkbox>Remember me</Checkbox>)}
+                    <a className="login-form-forgot" href="/forgot-password">
+                        Forgot password
+                    </a>
+                    <Button type="primary" htmlType="submit" className="login-form-button">
+                        Log in
+                    </Button>
+                    Or <a href="/register">register now!</a>
+                </Form.Item>
+            </Form>
         );
     }
 }
 
+
+const NewLoginForm = Form.create({ name: 'normal_login' })(LoginForm);
+
 function mapStateToProps(state) {
-    const { loggingIn } = state.authentication;
-    return {
-        loggingIn
-    };
+    const { user } = state
+    return { user }
 }
 
-const connectedLoginPage = connect(mapStateToProps)(LoginPage);
-export { connectedLoginPage as LoginPage }; 
+const LoginPage = connect(mapStateToProps)(NewLoginForm);
+export default LoginPage;
