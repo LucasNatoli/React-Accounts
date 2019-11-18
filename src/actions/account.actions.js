62243993d9c8d8a3e0d190c09e9c2917a625e5b0
Lucas Natoli <lucas.natoli@gmail.com>
2019-11-17 23:43:33 -0300

    code prettify

--- src/actions/account.actions.js
+++ src/actions/account.actions.js
@@ -7,15 +7,13 @@ export const accountActions = {
     login,
     logout,
     register,
-/*     getAll,
-    delete: _delete */
 };
 
-function login(username, password) {
+function login(email, password) {
     return dispatch => {
-        dispatch(request({ username }));
+        dispatch(request({ email: email }));
 
-        accountsService.login(username, password)
+        accountsService.login(email, password)
             .then(
                 user => { 
                     dispatch(success(user));
@@ -60,36 +58,3 @@ function register(user) {
     function success(user) { return { type: accountConstants.REGISTER_SUCCESS, user } }
     function failure(error) { return { type: accountConstants.REGISTER_FAILURE, error } }
 }
-/* 
-function getAll() {
-    return dispatch => {
-        dispatch(request());
-
-        accountsService.getAll()
-            .then(
-                users => dispatch(success(users)),
-                error => dispatch(failure(error.toString()))
-            );
-    };
-
-    function request() { return { type: accountConstants.GETALL_REQUEST } }
-    function success(users) { return { type: accountConstants.GETALL_SUCCESS, users } }
-    function failure(error) { return { type: accountConstants.GETALL_FAILURE, error } }
-}
-
-// prefixed function name with underscore because delete is a reserved word in javascript
-function _delete(id) {
-    return dispatch => {
-        dispatch(request(id));
-
-        accountsService.delete(id)
-            .then(
-                user => dispatch(success(id)),
-                error => dispatch(failure(id, error.toString()))
-            );
-    };
-
-    function request(id) { return { type: accountConstants.DELETE_REQUEST, id } }
-    function success(id) { return { type: accountConstants.DELETE_SUCCESS, id } }
-    function failure(id, error) { return { type: accountConstants.DELETE_FAILURE, id, error } }
-} */
