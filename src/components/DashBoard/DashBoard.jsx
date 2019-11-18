1293eabeb7b036bab7941ff48c302092bc4e3b4b
Lucas Natoli <lucas.natoli@gmail.com>
2019-11-17 23:43:53 -0300

    Dashboard revamp

--- src/components/DashBoard/DashBoard.jsx
+++ src/components/DashBoard/DashBoard.jsx
@@ -1,26 +1,23 @@
 import React from 'react'
-import { Layout, Menu, Icon } from 'antd'
-import { Link } from 'react-router-dom'
+import {
+  Layout,
+  //Menu, Icon 
+} from 'antd'
+//import { Link } from 'react-router-dom'
 import './DashBoard.css';
 
 class DashBoard extends React.Component {
   render() {
-
     const { Content, Header } = Layout;
-    
     return (
       <Layout className="dashboard">
-  
         <Layout>
-          <Header className="header">
-            <h1>HEADER</h1>
-            
+          <Header>
+            <div className="logo">Logo</div>
           </Header>
           <Content>
             <div className="content">
-
               <h1>Hello</h1>
-
             </div>
           </Content>
         </Layout>
