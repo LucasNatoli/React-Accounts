0f79723f9208787e357396b064d65e7e9fcf00db
Lucas Natoli <lucas.natoli@gmail.com>
2019-11-17 23:44:45 -0300

    RegisterPage revamp

--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,91 +1,196 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Input, Card } from 'antd';
-import { connect } from 'react-redux';
+import { Button, Form, Icon, Input } from 'antd';
+import './RegisterPage.css'
 
-import { accountActions } from '../../actions';
+class RegisterForm extends React.Component {
 
-class RegisterPage extends React.Component {
-    constructor(props) {
-        super(props);
+    state = {
+        confirmDirty: false,
+    };
 
-        this.state = {
-            user: {
-                firstName: '',
-                lastName: '',
-                username: '',
-                password: ''
-            },
-            submitted: false
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        const { name, value } = event.target;
-        const { user } = this.state;
-        this.setState({
-            user: {
-                ...user,
-                [name]: value
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
             }
         });
-    }
+    };
 
-    handleSubmit(event) {
-        event.preventDefault();
+    handleConfirmBlur = e => {
+        const { value } = e.target;
+        this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+    };
 
-        this.setState({ submitted: true });
-        const { user } = this.state;
-        const { dispatch } = this.props;
-        if (user.firstName && user.lastName && user.username && user.password) {
-            dispatch(accountActions.register(user));
+
+    compareToFirstPassword = (rule, value, callback) => {
+        const { form } = this.props;
+        if (value && value !== form.getFieldValue('password')) {
+            callback('Two passwords that you enter is inconsistent!');
+        } else {
+            callback();
         }
-    }
+    };
+
+    validateToNextPassword = (rule, value, callback) => {
+        const { form } = this.props;
+        if (value && this.state.confirmDirty) {
+            form.validateFields(['confirm'], { force: true });
+        }
+        callback();
+    };
+
+
+    /*     constructor(props) {
+            super(props);
+    
+            this.state = {
+                user: {
+                    firstName: '',
+                    lastName: '',
+                    username: '',
+                    password: ''
+                },
+                submitted: false
+            };
+    
+            this.handleChange = this.handleChange.bind(this);
+            this.handleSubmit = this.handleSubmit.bind(this);
+        }
+    
+        handleChange(event) {
+            const { name, value } = event.target;
+            const { user } = this.state;
+            this.setState({
+                user: {
+                    ...user,
+                    [name]: value
+                }
+            });
+        }
+    
+        handleSubmit(event) {
+            event.preventDefault();
+    
+            this.setState({ submitted: true });
+            const { user } = this.state;
+            const { dispatch } = this.props;
+            if (user.firstName && user.lastName && user.username && user.password) {
+                dispatch(accountActions.register(user));
+            }
+        } */
 
     render() {
-        const { registering  } = this.props;
-        const { user, submitted } = this.state;
+
+        const { getFieldDecorator } = this.props.form;
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 8 },
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 16 },
+            },
+        };
+        const tailFormItemLayout = {
+            wrapperCol: {
+                xs: {
+                    span: 24,
+                    offset: 0,
+                },
+                sm: {
+                    span: 16,
+                    offset: 8,
+                },
+            },
+        };
+
+
         return (
-            <Card className="register-card">
-                <h2>Crear Cuenta</h2>
-                <form name="form" onSubmit={this.handleSubmit}>
-                    <div className={'form-group mb-1' + (submitted && !user.firstName ? ' has-error' : '')}>
-                        <label htmlFor="firstName">Nombre y Apellido</label>
-                        <Input type="text" name="firstName" value={user.firstName} onChange={this.handleChange} />
-                        {submitted && !user.firstName &&
-                            <div className="help-block">Este campo es obligatorio</div>
-                        }
-                    </div>
-                    <div className={'form-group mb-1' + (submitted && !user.username ? ' has-error' : '')}>
-                        <label htmlFor="username">Email</label>
-                        <Input type="text" className="form-control" name="username" value={user.username} onChange={this.handleChange} />
-                        {submitted && !user.username &&
-                            <div className="help-block">Ingrese un Email valido</div>
-                        }
-                    </div>
-                    <div className={'form-group mb-1' + (submitted && !user.password ? ' has-error' : '')}>
-                        <label htmlFor="password">Contraseña</label>
-                        <Input type="password" className="form-control" name="password" value={user.password} onChange={this.handleChange} />
-                        {submitted && !user.password &&
-                            <div className="help-block">Ingrese una contraseña</div>
-                        }
-                    </div>
-                    <div className="form-group mt-2 ta-r">
-                        {registering && 
-                            <img alt="" src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-                        }
-                        <Link to="/login"  className="ant-btn ant-btn-default mr-1">Iniciar Sesion</Link>
-                        <button className="ant-btn ant-btn-primary">Crear Cuenta</button>
-                    </div>
-                </form>
-            </Card>
+            <Form {...formItemLayout} onSubmit={this.handleSubmit} className="register-form">
+                <Form.Item label="Full Name">
+                    {getFieldDecorator('fullName', {
+                        rules: [{ required: true, message: 'Please input your full name!' }],
+                    })(
+                        <Input
+                            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            placeholder="Full Name"
+                        />,
+                    )}
+                </Form.Item>
+                <Form.Item label="E-mail">
+                    {getFieldDecorator('email', {
+                        rules: [{ required: true, message: 'Please input your email!' }],
+                    })(
+                        <Input
+                            prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            placeholder="Email"
+                        />,
+                    )}
+                </Form.Item>
+                <Form.Item label="Phone Nbr.">
+                    {getFieldDecorator('phone', {
+                        rules: [{ required: true, message: 'Please input your phone number!' }],
+                    })(
+                        <Input
+                            prefix={<Icon type="phone" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            placeholder="Phone"
+                        />,
+                    )}
+                </Form.Item>
+                <Form.Item label="Password" hasFeedback>
+                    {getFieldDecorator('password', {
+                        rules: [
+                            {
+                                required: true,
+                                message: 'Please input your password!'
+                            },
+                            {
+                                validator: this.validateToNextPassword,
+                            },
+                        ],
+                    })(
+                        <Input.Password
+                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            placeholder="Password"
+                        />,
+                    )}
+                </Form.Item>
+                <Form.Item label="Confirm Password" hasFeedback>
+                    {getFieldDecorator('confirm-password', {
+                        rules: [
+                            {
+                                required: true,
+                                message: 'Please confirm the entered password!'
+                            },
+                            {
+                                validator: this.compareToFirstPassword,
+                            }
+                        ],
+                    })(
+                        <Input.Password
+                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            placeholder="Confirm Password"
+                            onBlur={this.handleConfirmBlur}
+                        />,
+                    )}
+                </Form.Item>
+                <Form.Item  {...tailFormItemLayout}>
+                    <Button type="primary" htmlType="submit" className="register-form-button">
+                        Register
+                    </Button>
+                    Or <a href="/login">Login now!</a>
+                </Form.Item>
+            </Form>
         );
     }
 }
 
+const RegisterPage = Form.create({ name: 'normal_login' })(RegisterForm);
+export default RegisterPage
+
+/*
 function mapStateToProps(state) {
     const { registering } = state.registration;
     return {
@@ -94,4 +199,4 @@ function mapStateToProps(state) {
 }
 
 const connectedRegisterPage = connect(mapStateToProps)(RegisterPage);
-export { connectedRegisterPage as RegisterPage };
+export { connectedRegisterPage as RegisterPage }; */
