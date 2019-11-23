6d9220660543a7a3e128af815ed0e8def596105a
Lucas Natoli <lucas.natoli@gmail.com>
2019-11-23 15:34:48 -0300

    emptyhome and action links. also maps state

--- src/components/DashBoard/DashBoard.jsx
+++ src/components/DashBoard/DashBoard.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
-import {
-  Layout,
-  //Menu, Icon 
-} from 'antd'
-//import { Link } from 'react-router-dom'
+import { Layout, } from 'antd'
+import HomeEmpty from './HomeEmpty'
+import logo from './420logo.png';
 import './DashBoard.css';
+import { accountActions } from '../../actions'
+import { connect } from 'react-redux'
 
 class DashBoard extends React.Component {
+  state = {
+    "loggedIn": false,
+    "user": {}
+  }
+
   render() {
+    const { authentication } = this.props
     const { Content, Header } = Layout;
     return (
       <Layout className="dashboard">
         <Layout>
           <Header>
-            <div className="logo">Logo</div>
+            <div className="logo"><img src={logo} height={48} /></div>
           </Header>
           <Content>
-            <div className="content">
-              <h1>Hello</h1>
-            </div>
+            <HomeEmpty loggedIn={authentication.loggedIn} userName={authentication.user ? authentication.user.fullName : 'none'} />
           </Content>
         </Layout>
       </Layout>
@@ -26,4 +30,9 @@ class DashBoard extends React.Component {
   }
 }
 
-export default DashBoard;
+function mapStateToProps(state) {
+  const { authentication } = state
+  return { authentication }
+}
+
+export default connect(mapStateToProps)(DashBoard);
---
+++ src/components/DashBoard/HomeEmpty.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Button, Empty } from 'antd'
+import './HomeEmpty.css'
+
+const HomeEmpty = ({ loggedIn, userName }) => (
+  <Empty
+    image="https://gw.alipayobjects.com/mdn/miniapp_social/afts/img/A*pevERLJC9v0AAAAAAAAAAABjAQAAAQ/original"
+    imageStyle={{
+      height: 92,
+    }}
+    className="home-empty"
+    description={
+      <span>
+        Customize <a href="#API">{userName}</a>
+      </span>
+    }
+  >
+    {!loggedIn ? <div>
+      <Button type="secondary" href="/register">Register</Button>
+      <Button type="primary" href="/login">LogIn</Button>
+    </div> : ''}
+    {loggedIn ? <div>
+      <Button type="secondary" href="/logout">LogOut</Button>
+      <Button type="secondary">CheckSession</Button>
+    </div> : ''}
+  </Empty>
+)
+/* 
+HomeEmpty.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    completed: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired
+  }).isRequired).isRequired,
+  onTodoClick: PropTypes.func.isRequired
+} */
+
+export default HomeEmpty
