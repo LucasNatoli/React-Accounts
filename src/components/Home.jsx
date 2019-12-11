c689a4b2ce2ff0dc58ac6aef2ab9b14d8d53d5eb
Lucas Natoli <lucas.natoli@gmail.com>
2019-12-11 00:14:12 -0300

    home prettify

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Layout, } from 'antd'
+import { Avatar, Layout, Popover } from 'antd'
 import HomeEmpty from './HomeEmpty'
-import logo from './assets/420logo.png';
 import './css/Home.css';
 import { accountActions } from '../actions'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 class Home extends React.Component {
   state = {
@@ -18,39 +18,54 @@ class Home extends React.Component {
     this.logoutUser = this.logoutUser.bind(this)
     this.checkStatus = this.checkStatus.bind(this)
   }
-  checkToken() {
+  checkToken(e) {
+    e.preventDefault()
     const { dispatch } = this.props;
     dispatch(accountActions.checkToken())
   }
-  logoutUser() {
+  logoutUser(e) {
+    e.preventDefault()
     const { dispatch } = this.props
     dispatch(accountActions.logout)
     this.props.history.push('/login')
   }
-  checkStatus() {
-    const { dispatch }  = this.props
+  checkStatus(e) {
+    e.preventDefault()
+    const { dispatch } = this.props
     dispatch(accountActions.getServerStatus())
 
   }
   render() {
     const { Content, Header } = Layout;
     const user = JSON.parse(localStorage.getItem('user'))
-    const loggedIn = (user!==null)
-    
+    const loggedIn = (user !== null)
+
+
+    const userMenu = (
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <Link className="nav-text" to="/user-account" title="Profile">Your Profile</Link>
+        <Link className="nav-text" to="/check-token" onClick={this.checkToken} title="check-token">Check Token</Link>
+        <Link className="nav-text" to="/check-status" onClick={this.checkStatus} title="check-token">Check Status</Link>
+        <Link className="nav-text" to="/logout" onClick={this.logoutUser} title="Logout">Logout</Link>
+      </div>
+    );
+
     return (
       <Layout className="home">
-          <Header>
-            <div className="logo"><img src={logo} height={48} alt="420"/></div>
-          </Header>
-          <Content>
-            <HomeEmpty 
-              loggedIn={loggedIn} 
-              checkTokenClick={this.checkToken}
-              checkStatusClick={this.checkStatus}
-              logoutClick={this.logoutUser}
-              userName={user ? user.fullname : 'none'} 
-            />
-          </Content>
+        <Header>
+          <div className="logo" style={{ display: 'flex' }}><h1>React Accounts</h1></div>
+          <div className="user-details">
+            <Popover placement="bottomRight" content={userMenu} title={loggedIn ? user.fullname : '?'} trigger="click">
+              <Avatar>{loggedIn ? user.fullname.charAt(0) : '?'}</Avatar>
+            </Popover>
+          </div>
+        </Header>
+        <Content>
+          <HomeEmpty
+            loggedIn={loggedIn}
+            userName={user ? user.fullname : 'none'}
+          />
+        </Content>
       </Layout>
     );
   }
--- src/components/HomeEmpty.jsx
+++ src/components/HomeEmpty.jsx
@@ -2,38 +2,23 @@ import React from 'react'
 import { Button, Empty } from 'antd'
 import './css/HomeEmpty.css'
 
-const HomeEmpty = ({ loggedIn, userName, checkTokenClick, logoutClick, checkStatusClick }) => (
+const HomeEmpty = ({ loggedIn, userName}) => (
   <Empty
     image="https://gw.alipayobjects.com/mdn/miniapp_social/afts/img/A*pevERLJC9v0AAAAAAAAAAABjAQAAAQ/original"
     imageStyle={{
       height: 92,
     }}
     className="home-empty"
-    description={
-      <span>
-        You are <a href="user-account">{userName}</a>
-      </span>
+    description={loggedIn ?
+      <span>You are <a href="user-account">{userName}</a></span> :
+      <span>I don't know you</span>
     }
   >
     {!loggedIn ? <div>
       <Button type="secondary" href="/register">Register</Button>
       <Button type="primary" href="/login">Log In</Button>
     </div> : ''}
-    {loggedIn ? <div>
-      <Button type="secondary" onClick={logoutClick}>Log Out</Button>
-      <Button type="secondary" onClick={checkTokenClick}>Check Token</Button>
-      <Button type="secondary" onClick={checkStatusClick}>Check Status</Button>
-    </div> : ''}
   </Empty>
 )
-/* 
-HomeEmpty.propTypes = {
-  todos: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    completed: PropTypes.bool.isRequired,
-    text: PropTypes.string.isRequired
-  }).isRequired).isRequired,
-  onTodoClick: PropTypes.func.isRequired
-} */
 
 export default HomeEmpty
